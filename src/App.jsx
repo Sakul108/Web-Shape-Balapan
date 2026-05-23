b214cd7f8009a44a9f77d56813e80bf0c13e0da8
Sakul <sakulkhatri108@gmail.com>
2026-05-23 12:51:26 +0545

    Fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
 import './index.css'
-
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import Vision from './components/Vision'
 import Team from './components/Team'
 import Stories from './components/Stories'
-
-import { Route, Routes } from 'react-router-dom'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <>
+   
+    <BrowserRouter basename="/Web-Shape-Balapan">
       <Navbar />
       <Routes>
-        <Route path="/"        element={<Home />}    />
-        <Route path="/vision"  element={<Vision />}  />
-        <Route path="/team"    element={<Team />}    />
+        <Route path="/" element={<Home />} />
+        <Route path="/vision" element={<Vision />} />
+        <Route path="/team" element={<Team />} />
         <Route path="/stories" element={<Stories />} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
